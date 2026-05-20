950d85185de3b16f17c9af05c0ea24ac3c4ef63b
Luiz Felipe <luizfelipeq00@gmail.com>
2026-05-20 19:19:44 -0300

    Arquivos JS

---
+++ js/ex002.js
@@ -0,0 +1,13 @@
+let estouDeFolga = true
+let temSol = true
+console.log("Vou ir a praia!", estouDeFolga && temSol)
+
+let canetaAzul = true
+let canetaPreta = false
+console.log("Vou escrever!", canetaAzul || canetaPreta)
+
+let testes = true
+console.log(!testes)
+
+let resultado = (5 > 4 && 20 < 15 || 9 == 9)
+console.log(resultado)
---
+++ js/ex003.js
@@ -0,0 +1,14 @@
+let velocidade = 81
+let velocidadeMaxima = 80
+
+console.log("Sua velocidade é " + velocidade + "km/h")
+
+if (velocidade > velocidadeMaxima){
+    console.log("Você passou e 80km/h e foi MULTADO!")
+}
+else{
+    console.log("Boa viagem!")
+}
+
+
+console.log("Dirija sempre com cuidado!")
