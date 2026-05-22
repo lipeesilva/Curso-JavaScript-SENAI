7b413ed66620c0eeff5c2d4db5e76e1c76a7264f
Luiz Felipe da Silva <luizfelipeq00@gmail.com>
2026-05-22 20:40:12 -0300

    Arquivos JS

---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.log("Executado pelo arquivo index.js")
+
+function somarNumeros (x, y){
+    console.log("Aqui é feito a soma dos números")
+    let soma = x + y
+    console.log(soma)
+}
+
+somarNumeros(5, 10)
